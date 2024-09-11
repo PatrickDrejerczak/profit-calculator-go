dc5341ab292bf84873b2c15f56dd271722b90b13
PatrickDrejerczak <drejerczak@aol.com>
2024-09-11 15:43:51 +0200

    added functions for inputs and calculations

--- profit_calculator.go
+++ profit_calculator.go
@@ -10,18 +10,28 @@ func main() {
 	var expenses float64
 	var taxRate float64
 
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Enter tax rate: ")
-	fmt.Scan(&taxRate)
+	revenue = getRequiredInput("Enter revenue: ", revenue)
+	expenses = getRequiredInput("Enter expenses: ", expenses)
+	taxRate = getRequiredInput("Enter tax rate: ", taxRate)
 
-	earningsBeforeTax := revenue - expenses
-	profit := earningsBeforeTax * (1 - taxRate/100)
-	profitRatio := profit / revenue
+	ebt, profit, profitRatio := calculateEarnings(revenue, expenses, taxRate)
 
-	fmt.Println("Earnings before tax: ", earningsBeforeTax)
+	fmt.Println("Earnings before tax: ", ebt)
 	fmt.Println("Profit: ", profit)
 	fmt.Println("Profit ratio: ", math.Round(profitRatio*100), "%")
+}
+
+func getRequiredInput(text string, value float64) float64 {
+	fmt.Print(text)
+	fmt.Scan(&value)
+
+	return value
+}
+
+func calculateEarnings(revenue float64, expenses float64, taxRate float64) (ebt float64, profit float64, profitRatio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	profitRatio = profit / revenue
+
+	return ebt, profit, profitRatio
 }
